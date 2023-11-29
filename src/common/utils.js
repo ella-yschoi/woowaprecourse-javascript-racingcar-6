03653b41cf8218b49434b39f4bb32f1f31b54d6e
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-29 21:13:22 +0900

    refactor: Util 함수 중 에러를 던지는 메서드 수정

--- src/common/utils.js
+++ src/common/utils.js
@@ -2,8 +2,8 @@ import { Console, Random } from '@woowacourse/mission-utils';
 
 const printMessage = (message) => Console.print(message);
 
-const throwError = (message, condition = true) => {
-  if (condition) throw new Error(message);
+const throwError = (message) => {
+  throw new Error(message);
 };
 
 const generateRandomNumber = (min, max) => Random.pickNumberInRange(min, max);
