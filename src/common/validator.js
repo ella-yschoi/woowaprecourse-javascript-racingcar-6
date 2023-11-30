0d84f0f00176fdc7a35e8527463064d58a36eaa2
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-30 17:31:05 +0900

    fix: validator에서 시도 횟수 리턴이 되지 않았던 부분 해결

--- src/common/validator.js
+++ src/common/validator.js
@@ -4,17 +4,17 @@ import { isNumeric, isSeperatedByComma, isUniqueValue } from './utils';
 const isValidCarName = (input) => {
   const carNames = input.split(',').map(name => name.trim().toUpperCase());
   return (
-    isSeperatedByComma(carNames)
-    && carNames.length >= GAME.zero
+    isSeperatedByComma(input)
+    && carNames.length > GAME.zero
     && carNames.every(name => name.length <= GAME.max_length)
     && !carNames.includes('')
     && isUniqueValue(carNames.join(','))
-  )
-}
+  );
+};
 
 const isValidTryCount = (input) => {
-  input.toString().length >= GAME.min_try_count
+  return input.toString().length >= GAME.min_try_count
   && isNumeric(input)
-}
+};
 
-export { isValidCarName, isValidTryCount };
+export { isValidCarName, isValidTryCount }
