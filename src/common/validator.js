7d1594dffcdbba0fbcb66144bf39626158c688dc
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-12-03 20:36:53 +0900

    refactor: 시도 횟수 및 자동차 이름 유효성 검증하는 메서드 수정

--- src/common/validator.js
+++ src/common/validator.js
@@ -1,20 +1,24 @@
 import { GAME } from './constants.js';
-import { isUniqueValue, isSeperatedByComma, isNumeric } from './utils.js';
+import { isNumeric, isSeperatedByComma, isUniqueValue } from './utils.js';
 
 const isValidCarName = (input) => {
+  if (!input) {
+    return false;
+  }
+
+  if (!isSeperatedByComma(input)) {
+    return false;
+  }
+
   const carNames = input.split(',').map(name => name.trim().toUpperCase());
   return (
-    isSeperatedByComma(input)
-    && carNames.length > GAME.zero
-    && carNames.every(name => name.length <= GAME.max_length)
-    && !carNames.includes('')
+    carNames.every(name => name.length <= GAME.max_length && name)
     && isUniqueValue(carNames.join(','))
-  );
-};
+  )
+}
 
 const isValidTryCount = (input) => {
-  return input.toString().length >= GAME.min_try_count
-  && isNumeric(input)
-};
+  return isNumeric(input) && input >= GAME.min_try_count;
+}
 
 export { isValidCarName, isValidTryCount }
